be8d3484da649e2adb2f2edbc552ac2369722f77
Samuil Velikov <94323419+SvPhxc@users.noreply.github.com>
2022-07-17 19:38:26 +0300

    Add files via upload

---
+++ Python OOP/Static and Class Methods/New/Shop.py
@@ -0,0 +1,12 @@
+class Shop:
+    def __init__(self, name: str, type: str, capacity: int):
+        self.name = name
+        self.type = type
+        self.capacity = capacity
+        self.items = {}
+
+    @classmethod
+    def small_shop(cls, name: str, type: str):
+        return cls(name, type, 10)
+
+    def add_item(self, item_name:str):
